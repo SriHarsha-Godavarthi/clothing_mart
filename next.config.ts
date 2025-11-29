8d163cbc72052f20cd7c471731062e49804f7f86
SriHarsha Godavarthi <sriharsha.godavarthi232@gmail.com>
2025-11-29 09:25:42 -0600

    Configure Next.js project with TypeScript and image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {protocol: 'https', hostname: 'fakestoreapi.com'},
+      {protocol: 'https', hostname: 'fakestoreapiserver.reactbd.org'},
+      {protocol: 'https', hostname: 'i.ibb.co'},
+      {protocol: 'https', hostname: '**.amazonaws.com'},
+    ],
+  }
 };
 
 export default nextConfig;
